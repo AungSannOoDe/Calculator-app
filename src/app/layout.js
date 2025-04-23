0ef305886bb4bcd7380ea118046b4d2052f598f6
AungSannOoDe <theflash1321@gmail.com>
2025-04-23 22:53:05 +0630

    add meta tag  in  Calculator

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,8 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta name="viewport" 
+      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"/>
       <body>
         {children}
       </body>
